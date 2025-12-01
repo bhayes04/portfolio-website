573a753b3d9c366de007e893c450d3c433db301f
Bryce <brycehayes04@gmail.com>
2025-11-30 23:27:40 -0600

    Fix #2

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,13 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/portfolio-website/',
+    build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        gallery: resolve(__dirname, 'art_gallery.html'),
+        projects: resolve(__dirname, 'projects.html'), // Changed from sb.html
+      }
+    }
+  }
 })
